6b63d7e86bab4cecae799ccb5b9c65d444232159
stealthomikey <115486033+stealthomikey@users.noreply.github.com>
2025-05-22 20:38:06 +0100

    change db settings

--- config/db.js
+++ config/db.js
@@ -4,9 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
